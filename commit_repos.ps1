dccefd086d0f284880b3894e153db64a9aa98476
Damien Beard <damienp.beard@gmail.com>
2015-04-09 15:51:26 -0700

    GitHub-Automation-with-PowerShell.Name 04/09/2015 15:51:26

---
+++ commit_repos.ps1
@@ -0,0 +1,15 @@
+﻿ #This file moves to my GitHub directory and updates all of my repositories. It is executed 
+ 
+ $date = Get-Date
+ 
+ cd "C:\Users\Damo\Documents\GitHub"
+ 
+ Get-ChildItem | ?{ $_.PSIsContainer } |  ForEach-Object {
+    cd $_.FullName
+
+    git add --all .
+    git commit -m "$_.Name $date"
+    git push
+    }
+
+Write-Host "Task Completed"
