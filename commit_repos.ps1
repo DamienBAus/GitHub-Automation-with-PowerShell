c7377d61ad0534b38b308f29d4d9ebaf5a337825
Damien Beard <damienp.beard@gmail.com>
2015-04-09 16:30:36 -0700

    GitHub-Automation-with-PowerShell.Name 04/09/2015 16:30:36

--- commit_repos.ps1
+++ commit_repos.ps1
@@ -1,13 +1,15 @@
 ﻿ #This file moves to my GitHub directory and updates all of my repositories. It is executed by an Automated Task #
  
  $date = Get-Date
- 
- cd "C:\Users\Damo\Documents\GitHub"
+ $name = [Environment]::UserName
+ $gitHubUserName = "DamienBAus" # Change this line to match your GitHub Username #
+
+ cd "C:\Users\$name\Documents\GitHub"
  
  Get-ChildItem | ?{ $_.PSIsContainer } |  ForEach-Object {
     cd $_.FullName
 
-    git remote set-url origin ssh://git@github.com/DamienBAus/$_.git
+    git remote set-url origin ssh://git@github.com/$gitHubUserName/$_.git
     git add --all .
     git commit -m "$_.Name $date"
     git push
